80918c30d3db68543c5af66aa3abfdc50c53606f
vasanthakumar-s <s.vasanthakumar1112@gmail.com>
2024-02-17 11:09:53 +0530

    Binary Search - Order Agnostic

---
+++ BinarySearch/BinarySearch_OrderAgnostic.java
@@ -0,0 +1,55 @@
+package Algorithm.BinarySearch;
+
+import java.util.Scanner;
+
+public class BinarySearch_OrderAgnostic{
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the length of the array: ");
+        int length = input.nextInt();
+        int array[] = new int[length];
+        System.out.print("Enter the elements: ");
+        for(int index=0; index<length; index++){
+            array[index] = input.nextInt();
+        }
+        System.out.print("Enter the target: ");
+        int target = input.nextInt();
+        System.out.println(binarySearch(array,target));
+    }
+    static int binarySearch(int array[],int target){
+        int start = 0;
+        int end = array.length-1;
+        boolean isAscending = false;
+        if(array[start]<array[end]){
+            isAscending = true;
+        }
+
+        while(start <= end){
+            int mid = (start+end)/2;
+            if(target == array[mid]){
+                return mid;
+            }
+            else if(isAscending == true){
+                if(target<array[mid]){
+                    end = mid-1;
+                }
+                else{
+                    start = mid+1;
+                }
+            }
+            else{
+                if(target>array[mid]){
+                    end = mid -1;
+                }
+                else{
+                    start = mid+1;
+                }
+            }
+        }
+        return -1;
+    }
+}
+
+// 7 6 5     4     3 2 1 
+// s                   e
+// 2
